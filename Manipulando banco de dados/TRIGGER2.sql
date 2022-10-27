a461459fa4dc9c49c14f05fac5db1fd53556be34
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-10-27 11:13:03 -0300

    Adicionando TSQL

--- Manipulando banco de dados/TRIGGER2.sql
+++ Manipulando banco de dados/TRIGGER2.sql
@@ -21,9 +21,11 @@ END;
 
 select * from t1
 
+insert into t1 values (1,2,3,4)
+
 update t1 set c1 = 4 where id = 3
 
-insert into t1 values (1,2,3,4)
+
 
 CREATE TABLE welcome (
     id int IDENTITY(1,1) NOT NULL,
@@ -42,31 +44,39 @@ select * from registrosImpressoes
 
 UPDATE [dbo].[welcome]
    SET 
-      [cracha_impresso] = 2
+      [cracha_impresso] = 6,
+	  nome = 'GG'
  WHERE cpf = 'CPF2'
 
 
 
-CREATE TRIGGER novaImpressao
+CREATE TRIGGER DMLnovaImpressao
 ON [dbo].[welcome]
 AFTER UPDATE
 AS
 IF ( UPDATE(cracha_impresso) )
 BEGIN
-	DECLARE @nome nvarchar(250), @cpf nvarchar (14), @sys_user char(250)
+	DECLARE @cpf nvarchar (14)
 	
-	SELECT @nome = nome, @cpf = cpf FROM INSERTED
+	SELECT @cpf = cpf FROM INSERTED
 
-    SELECT @sys_user = SYSTEM_USER; 
+    --SELECT @sys_user = SYSTEM_USER; 
 
 	--SELECT @novo = (SELECT cpf from [dbo].[welcome] as A where A.id = @id)
 
 	--PRINT @cpf
 	--PRINT @sys_user
 
-	INSERT INTO [dbo].[registrosImpressoes] 
-		(dt_server, nome, cpf, usuario, origin)
+	INSERT INTO [SUCOS_VENDAS].[dbo].[registrosImpressoes] 
+		(dt_server, origin, usuario, Command, cpf, OldRowData, NewRowData)
 		VALUES
-			( SYSDATETIME(), @nome, @cpf, @sys_user, 'WELCOME')
+			( SYSDATETIME(),
+			  'WELCOME',
+			  SUSER_SNAME(),
+			  'UPDATE', 
+			  @cpf, 
+			  (SELECT * FROM DELETED FOR JSON PATH, WITHOUT_ARRAY_WRAPPER),
+			  (SELECT * FROM INSERTED FOR JSON PATH, WITHOUT_ARRAY_WRAPPER)
+			  )
 END
 
